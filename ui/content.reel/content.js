2d5073120287591d916521d6026c44273ad7963d
particalchen <noreply>
2015-01-22 01:43:13 +0000

    Add component content

---
+++ ui/content.reel/content.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/content.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Content
+ * @extends Component
+ */
+exports.Content = Component.specialize(/** @lends Content# */ {
+    constructor: {
+        value: function Content() {
+            this.super();
+        }
+    }
+});
